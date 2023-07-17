8b3bc8087e692bf0561aa8c3a4d76e0a4053a318
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2023-07-16 23:11:22 -0400

    fevents schema impl - wip

--- src/rca-demo/tests/test_rca_demo.py
+++ src/rca-demo/tests/test_rca_demo.py
@@ -1,5 +1,14 @@
 from rca_demo import __version__
 
+from rca_demo.fevent_dataset import FulfillmentEventType, load_event_types, FULFILLMENT_EVENT_TYPES_SCHEMA
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_load_event_types():
+    event_types = load_event_types(FULFILLMENT_EVENT_TYPES_SCHEMA)
+
+    assert event_types is not None
+    assert isinstance(event_types, type(list[Fulfillm
